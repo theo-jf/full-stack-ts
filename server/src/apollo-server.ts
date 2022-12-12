b436e21a20b89bc8c114d75191028a79db746354
Theo Janke-Furman <tjankefurman@gmail.com>
2022-12-12 16:38:12 -0600

    Resolvers for CurrentUser added to apollo-server.ts

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -8,7 +8,7 @@ import * as express from "express"
 import { Server } from "http"
 import Db from "./db"
 export async function createApolloServer(
-  _db: Db,
+  db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
@@ -33,8 +33,29 @@ export async function createApolloServer(
       reason: String!
     }
   `
+  const resolvers = {
+    Query: {
+      currentUser: () => {
+        return {
+          id: "123",
+          name: "John Doe",
+          handle: "johndoe",
+          coverUrl: "",
+          avatarUrl: "",
+          createdAt: "",
+          updatedAt: "",
+        }
+      },
+      suggestions: () => {
+        return []
+      },
+    },
+  }
+
   const server = new ApolloServer({
     typeDefs,
+    resolvers,
+    context: () => ({ db }),
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
     ],
