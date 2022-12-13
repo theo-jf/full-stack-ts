18c22f5ca97817d6931e4d31cf65fc358306b6a5
Theo Janke-Furman <tjankefurman@gmail.com>
2022-12-13 15:26:41 -0600

    Resolver context added

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,5 +1,5 @@
 // import queryTwitterResolvers from "./resolvers/Query"
-// import Db from "./db"
+import Db from "./db"
 import { Resolvers } from "./resolvers-types.generated"
 import Query from "./resolvers/Query"
 
@@ -12,13 +12,12 @@ import Query from "./resolvers/Query"
 //     }
 // }
 
-const resolvers: Resolvers = {
+export interface TwitterResolverContext {
+    db: Db
+}
+
+const resolvers: Resolvers<TwitterResolverContext> = {
     Query
 };
     
 export default resolvers;
-    
-// export interface TwitterResolverContext {
-//     db: Db
-// }
-    
