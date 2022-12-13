73175169512521e879b4031e8fa256a1632d4a15
Theo Janke-Furman <tjankefurman@gmail.com>
2022-12-13 15:15:17 -0600

    added suggestions

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,18 +1,24 @@
-import queryTwitterResolvers from "./resolvers/Query"
-import Db from "./db"
+// import queryTwitterResolvers from "./resolvers/Query"
+// import Db from "./db"
 import { Resolvers } from "./resolvers-types.generated"
+import Query from "./resolvers/Query"
 
-export default function createResolvers(): Resolvers{
+// export default function createResolvers(): Resolvers{
 
-    return  {
-        const resolvers: Resolvers<TwitterResolverContext> = {
+//     return  {
+//         const resolvers: Resolvers<TwitterResolverContext> = {
             
-        }
-    }
-}
+//         }
+//     }
+// }
+
+const resolvers: Resolvers = {
+    Query
+};
     
+export default resolvers;
     
-export interface TwitterResolverContext {
-    db: Db
-}
+// export interface TwitterResolverContext {
+//     db: Db
+// }
     
--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -13,7 +13,23 @@ const queryTwitterResolvers: QueryResolvers = {
       }
     },
     suggestions: () => {
-      return []
+      return [
+        {
+            name: "TypeScript Project",
+            handle: "TypeScript",
+            avatarUrl: "http://localhost:3000/static/ts-logo.png",
+            reason: "Because you follow @MichaelLNorth",
+            id: "1",
+        },
+        {
+            name: "jQuery",
+            handle: "jquery",
+            avatarUrl:
+            "http://localhost:3000/static/jquery-logo.jpeg",
+            reason: "Because you follow @FrontendMasters",
+            id: "2",
+        },
+      ]
     },
   }
   export default queryTwitterResolvers
