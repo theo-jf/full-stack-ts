5a9c033a7f2972ceade696219df1076e51db8a72
Theo Janke-Furman <tjankefurman@gmail.com>
2022-12-13 15:47:45 -0600

    Edited Query.ts to pull from the database

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -2,35 +2,16 @@ import { QueryResolvers } from "../resolvers-types.generated"
 import { TwitterResolverContext } from "resolvers" 
 
 const queryTwitterResolvers: QueryResolvers<TwitterResolverContext> = {
-    currentUser: () => {
-      return {
-        id: "123",
-        name: "John Doe",
-        handle: "johndoe",
-        coverUrl: "",
-        avatarUrl: "",
-        createdAt: "",
-        updatedAt: "",
-      }
-    },
-    suggestions: (_, __, { db: _db }) => {
-      return [
-        {
-            name: "TypeScript Project",
-            handle: "TypeScript",
-            avatarUrl: "http://localhost:3000/static/ts-logo.png",
-            reason: "Because you follow @MichaelLNorth",
-            id: "1",
-        },
-        {
-            name: "jQuery",
-            handle: "jquery",
-            avatarUrl:
-            "http://localhost:3000/static/jquery-logo.jpeg",
-            reason: "Because you follow @FrontendMasters",
-            id: "2",
-        },
-      ]
+  currentUser: (_, __, { db }) => {
+    const [firstUser] = db.getAllUsers();
+    if (!firstUser)
+      throw new Error(
+        'currentUser was requested, but there are no users in the database'
+      );
+    return firstUser;
+  },
+    suggestions: (_, __, { db }) => {
+      return db.getAllSuggestions();
     },
   }
   export default queryTwitterResolvers
